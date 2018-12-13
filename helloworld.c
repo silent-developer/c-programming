edc9bddd6da3eba5acc005b83b0d49e2307ff28e
silent-developer <45838956+silent-developer@users.noreply.github.com>
2018-12-13 19:20:12 +0530

    tried to add two numbers

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<conio.h>
 void main()
-{
+{ 
+    int a,b;
   printf("Hello the coding world, I am here");
-}
+  printf("Enter 2 numbers to add:");
+    scanf("%d %d",&a,&b);
+  printf("Sum is %d",a+b);
+ }

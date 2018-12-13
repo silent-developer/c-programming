c83c51cc74a5526daa4766c1b1d5dcd0ad3949b9
silent-developer <45838956+silent-developer@users.noreply.github.com>
2018-12-14 01:35:12 +0530

    updated the issue #4 opened by harishpillai

--- helloworld.c
+++ helloworld.c
@@ -3,13 +3,15 @@
 void main()
 { 
     int a,b,i;
-  printf("Hello the coding world, I am here");
-    printf("This is a pull request from my gmail harishpillai account");
-  printf("Enter 2 numbers to add:");
+  printf("\n Hello the coding world, I am here");
+    printf("\n This is a pull request from my gmail harishpillai account");
+  printf("\n Enter 2 numbers to add:");
     scanf("%d %d",&a,&b);
     for(i=0;i<5;i++)
-        printf("This is %d line",i);
-    printf("Sum is %d",a+b);
-    printf("Multiplication of 2 numbers is:%d",a*b);
-    printf("After harishpillai gave pull request this is now the state of the file");
+        printf("\n This is %d line",i);
+    printf("\n Sum is %d",a+b);
+    printf("\n Multiplication of 2 numbers is:%d",a*b);
+    printf("\n After harishpillai gave pull request this is now the state of the file");
+    printf("\n This is the version after issue added by harishpillai.")
+    
 }
